e61abc7a8cf0c998a16196f127300d977ba444a3
harryless <manseur_aghiles@hotmail.fr>
2018-11-16 14:25:03 +0100

    test

--- connexion.php
+++ connexion.php
@@ -1,17 +1,47 @@
 <!DOCTYPE html>
 <html>
-  <head>
+    <head>
     <meta charset="utf-8">
-
+    <link rel="stylesheet" href="css/connexion.css">
     <title>Connexion</title>
-    <div class="">
-      <h2>Identifiant:</h2>
-      <input type="text" name="login" value="Saisissez votre Identifiant">
-      <h2>Mot de passe:</h2>
-      <input type="text" name="passeword" value="Saisissez votre mot de passe">
-    </div>
+    </head>
+    <form class="connexion" action="" method="post">
+      <div class="identifiant">
+        <h2>Identifiant:</h2>
+        <input type="text" name="login" value="">
 
-  </head>
+      </div>
+      <div class="mot de passe">
+        <h2>Mot de passe:</h2>
+        <input type="text" name="passeword" value="">
+      </div>
+      <input type="submit" name="valider" value="Connexion">
+    </form>
+    <?php
+    if (isset($_POST['valider'])) {
+      $database= new PDO('mysql:host=localhost;dbname=projetBoissons','root','');
+      $login = $_POST['login'];
+			$motDePasse = $_POST['passeword'];
+      $sql = "SELECT login,motDePasse FROM utilisateur WHERE login == '$login'";
+      $results=$database->query($sql);
+      if ($results) {
+          echo "string";
+      }
+      else {
+        echo "query";
+        }
+
+      }
+
+
+
+     ?>
+
+
+
+
+
+
   <body>
 
   </body>
