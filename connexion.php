889159375c97136cbfafd0e96bdb34fec51f6530
harryless <manseur_aghiles@hotmail.fr>
2018-11-16 12:26:21 +0100

    debut projet

--- connexion.php
+++ connexion.php
@@ -4,8 +4,12 @@
     <meta charset="utf-8">
 
     <title>Connexion</title>
-
-    <title></title>
+    <div class="">
+      <h2>Identifiant:</h2>
+      <input type="text" name="login" value="Saisissez votre Identifiant">
+      <h2>Mot de passe:</h2>
+      <input type="text" name="passeword" value="Saisissez votre mot de passe">
+    </div>
 
   </head>
   <body>
