4de0a520d6d870e63d3c06410cce42898e6d3f0b
benabbou <benabbou-abdelwahed@hotmail.com>
2018-11-19 15:53:46 +0100

    attta

--- accueil.php
+++ accueil.php
@@ -24,7 +24,7 @@ echo "bonjour monsieur  : ".$_SESSION['nom']." ".$_SESSION['prenom'];
     </br></br></br></br>
 
   <?php
-  foreach ($Recettes as $recette){
+ foreach ($Recettes as $recette){
 	//echo "<tr>\n";
       $titre = $recette['titre'];
       echo "<li style='display:inline-block;'>";
@@ -46,17 +46,26 @@ echo "bonjour monsieur  : ".$_SESSION['nom']." ".$_SESSION['prenom'];
   }
 
 $login=$_SESSION['login'];
-  if (isset($_GET['titre'])) {
+ if (isset($_GET['titre'])) {
+$titreRecette=$_GET['titre'];
+
     $database= new PDO('mysql:host=localhost;dbname=projetBoissons','root','root');
     $idUtilisateur="SELECT id_utilisateur FROM utilisateur WHERE login like '$login'";
-    $database->query($idUtilisateur);
+    $res=$database->query($idUtilisateur)->fetch();
+
+
+   $id_user=$res['id_utilisateur'];
+
+
     // $sql = "SELECT id_utilisateur FROM utilisateur u,panier p
     //  WHERE login like '$login' AND u.id_utilisateur==p.id_utilisateur ";
-    $sql="INSERT INTO panier(id_utilisateur,titre_recette)
-    VALUES(1,'grgr')";
-    $results=$database->query($sql);
+$database->query("INSERT INTO recette(titre,id_utlisateur)
+    VALUES('$titreRecette',$id_user)");
 
-  }
+
+
+}
+
 //
 // //on recupere la liste des super categorie
 //   foreach ($Hierarchie as $key =>$aliment){
