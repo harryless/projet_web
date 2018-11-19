396a37ee5cdc8af2475527916c335446fe83270d
benabbou <benabbou-abdelwahed@hotmail.com>
2018-11-19 11:59:25 +0100

    affichage alliment debut

--- accueil.php
+++ accueil.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html >
   <head>
+
     <meta charset="utf-8">
     <title>Accueil</title>
 
@@ -13,9 +14,45 @@
 <?php
 echo "bonjour monsieur  : ".$_SESSION['nom']." ".$_SESSION['prenom'];
  ?>
-
+</br></br></br></br>
+ </br></br></br></br>
   </head>
   <body>
+  </br></br></br></br>
+    </br></br></br></br>
+  <?php include 'Donnees.inc.php'; ?>
+
+  <?php
+  foreach ($Recettes as $recette){
+
+      $titre = $recette['titre'];
+      echo "<li>";
+      echo '<img src="Photos/'.$titre.'.jpg" alt="" width="100" height="100">';
+
+      echo "</li>";
+
+
+  }
+//
+// //on recupere la liste des super categorie
+//   foreach ($Hierarchie as $key =>$aliment){
+//
+//       foreach ($aliment as $key =>$value) {
+//
+//           foreach ($value as $key => $tmp) {
+//
+//           if($key=='super-categorie'){
+//               echo "<li>";
+//               echo "".$tmp;
+//               echo "</li>";
+//             }
+//
+//           }
+//       }
+//
+//   }
+
+?>
 
   </body>
 </html>
