49690309863d47cc45fddb64d53067204fa9d3d3
harryless <manseur_aghiles@hotmail.fr>
2018-11-22 16:11:24 +0100

    maj

--- panier.php
+++ panier.php
@@ -7,6 +7,7 @@
     <meta charset="utf-8">
     <title>Accueil</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -64,10 +65,9 @@
       $titreRecetteAsupp=$_GET['titre'];
       $suppRecette="DELETE FROM recette WHERE   titre like '$titreRecetteAsupp' ";
       $database->query($suppRecette)->fetch();
-
+      header('Location:panier.php');
 
     }
-
 
     ?>
       </div>
