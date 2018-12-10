9a87088ab7e792455c92c601bccbf08860998557
benabbou <benabbou-abdelwahed@hotmail.com>
2018-12-10 15:57:38 +0100

    recherche

--- aliment.php
+++ aliment.php
@@ -31,151 +31,81 @@ echo "bonjour monsieur  : ".$_SESSION['nom']." ".$_SESSION['prenom'];
   </br></br></br></br>
     </br></br></br></br>
 
-  <?php
-
-//fonction qui returne la liste des aliments
-
-
-foreach ($Hierarchie as $key1 =>$aliment){
-
-    foreach ($aliment as $key2 =>$value) {
-
-        foreach ($value as $key3 => $tmp) {
-          if($key1!="Aliment" ){
-
-            $tabAliments[]=$tmp;
-          //  echo "</br>".$tmp;
-          }
-        }
-      }
-    }
-
-
-?>
 <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" />
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 <script type="text/javascript" src="http://code.jquery.com/ui/1.10.1/jquery-ui.min.js"></script>
-
-
-               <?php
-
-               function tabDeRecettes($ali){
-
-                  foreach ($GLOBALS['Recettes'] as $GLOBALS['key1'] =>  $GLOBALS['value1'])  {
-                       foreach ($GLOBALS['value1'] as $GLOBALS['key2']  => $GLOBALS['value2']) {
-                         foreach ($GLOBALS['value2'] as $GLOBALS['key3'] => $GLOBALS['value3']) {
-
-                              if ($GLOBALS['key2']=='index' && $GLOBALS['value3']==$ali) {
-
-                                  echo  $GLOBALS['value1']["titre"];
-
-                                }
-
-                       }
-                     }
-                   }
-
-//return $ali;
-              }
-
-//------------------------------------------------------------
-function sousCategorie($aliment){
-  foreach ($GLOBALS['Hierarchie'] as $GLOBALS['key1'] =>  $GLOBALS['value1'])  {
-       foreach ($GLOBALS['value1'] as $GLOBALS['key2']  => $GLOBALS['value2']) {
-         foreach ($GLOBALS['value2'] as $GLOBALS['key3'] => $GLOBALS['value3']) {
-
-              if ($GLOBALS['key1']==$aliment && $GLOBALS['key2']=="sous-categorie") {
-
-                //  $tab[]= $GLOBALS['value3'];
-                  echo $GLOBALS['value3']."</br>";}
-
-
-       }
-     }
-   }
-return $tab;
-}
-$ali[]=sousCategorie("Légume");
-
-  //sousCategorie($GLOBALS['value3']);
-              ?>
-
-
-
-
-
-
-
-
-
-
 
 
    <script>
    function tabDAliments(){
        var tab = new Array() ;
 
-<?php
+        <?php
+
+               foreach ($GLOBALS['Hierarchie'] as $GLOBALS['key1'] =>  $GLOBALS['value1']){
+                 foreach ($GLOBALS['value1'] as $GLOBALS['key2']  => $GLOBALS['value2']){
+                   foreach ($GLOBALS['value2'] as $GLOBALS['key3'] => $GLOBALS['value3']){
+
+                     if($GLOBALS['key1']!="aliment" ){
+                       echo "tab= \"".str_replace("'", "", $GLOBALS['key1'])."\" ;"."\r\n" ;
+                     }
+
+                      }
+                    }
+                  }
+
+                 echo "return tab"."\r\n" ;
+
+        ?>
 
 
-
-       foreach ($GLOBALS['Hierarchie'] as $GLOBALS['key1'] =>  $GLOBALS['value1'])  {
-         foreach ($GLOBALS['value1'] as $GLOBALS['key2']  => $GLOBALS['value2']) {
-           foreach ($GLOBALS['value2'] as $GLOBALS['key3'] => $GLOBALS['value3']) {
-
-             if($GLOBALS['key1']!="aliment" ){
-              // echo "tab= \"".str_replace("'", "", $GLOBALS['key1'])."\" ;"."\r\n" ;
-
-
-             }
-
-              }
-            }
-          }
-
-      //   echo "return tab"."\r\n" ;
-
-
-?>
-
-    // $key = "" ;
-    // while($key != 'Aliment'){
-    //
-    //     $key = key($Hierarchie) ;
-    //     $key = str_replace("'", "", $key) ;
-    //     echo "res[\"".$key."\"] = new Array() ;"."\r\n" ;
-    //     echo "res[\"".$key."\"]['sous-categorie'] = new Array() ;"."\r\n" ;
-    //     echo "res[\"".$key."\"]['super-categorie'] = new Array() ;"."\r\n" ;
-    //     $pos = 0 ;
-    //     foreach ($Hierarchie[$key]['sous-categorie'] as $ssc){
-    //         echo "res[\"".$key."\"]['sous-categorie'][".$pos."] = \"".str_replace("'", "", $ssc)."\" ;"."\r\n" ;
-    //         $pos++ ;
-    //     }
-    //     $pos = 0 ;
-    //     foreach ($Hierarchie[$key]['super-categorie'] as $spc){
-    //         echo "res[\"".$key."\"]['super-categorie'][".$pos."] = \"".str_replace("'", "", $spc)."\" ;"."\r\n" ;
-    //         $pos++ ;
-    //     }
-    //     next($Hierarchie) ;
-    // }
-    // echo "return res"."\r\n" ;
    }
+   //
+   // function recupRecettes(){
+   //                 var res = new Array() ;
+   //                 <?php
+   //                 $key=0;
+   //                 while($key<107){
+   //                     $key = key($Recettes) ;
+   //                     echo "res[".$key."] = new Array()"."\r\n" ;
+   //                     echo "res[".$key."][\"titre\"] = \"".str_replace("\"", '\"', $Recettes[$key]['titre'])."\" ; "."\r\n" ;
+   //                     echo "res[".$key."][\"ingredients\"] = \"".str_replace("\"", '\"',$Recettes[$key]['ingredients'])."\" ; "."\r\n" ;
+   //                     echo "res[".$key."][\"preparation\"] = \"".str_replace("\"", '\"',$Recettes[$key]['preparation'])."\" ; "."\r\n" ;
+   //                     echo "res[".$key."][\"index\"] = new Array() ; "."\r\n" ;
+   //                     $numIng = 0 ;
+   //                     foreach ($Recettes[$key]['index'] as $ing) {
+   //                         echo "res[".$key."][\"index\"][".$numIng."] = \"".str_replace("'","",$ing)."\" ; "."\r\n" ;
+   //                         $numIng++ ;
+   //                     }
+   //                     next($Recettes) ;
+   //                 }
+   //
+   //                 ?>
+   //                 return res ;
+   //             }
+
    function recupRecettes(){
       var res = new Array() ;
       <?php
-  
-        foreach ($Recettes as $key => $value) {
-          // code...
 
-          echo "res[".$key."] = new Array()"."\r\n" ;
-          echo "res[".$key."][\"titre\"] = \"".str_replace("\"", '\"', $Recettes[$key]['titre'])."\" ; "."\r\n" ;
-          echo "res[".$key."][\"index\"] = new Array() ; "."\r\n" ;
+      foreach ($GLOBALS['Recettes'] as $GLOBALS['key1'] =>  $GLOBALS['value1'])  {
+           foreach ($GLOBALS['value1'] as $GLOBALS['key2']  => $GLOBALS['value2']) {
+             foreach ($GLOBALS['value2'] as $GLOBALS['key3'] => $GLOBALS['value3']) {
+
+          echo "res[".$GLOBALS['key1']."] = new Array()"."\r\n" ;
+          if($GLOBALS['key2']=="titre"){
+          echo "res[".$GLOBALS['key1']."][\"titre\"] = \"".str_replace("\"", '\"', $GLOBALS['value2'])."\" ; "."\r\n" ;}
+          echo "res[".$GLOBALS['key1']."][\"index\"] = new Array() ; "."\r\n" ;
           $numIng = 0 ;
-          foreach ($Recettes[$key]['index'] as $ing) {
-              echo "res[".$key."][\"index\"][".$numIng."] = \"".str_replace("'","",$ing)."\" ; "."\r\n" ;
-              $numIng++ ;
+          if ($GLOBALS['key2']=="index"){
+            foreach ($GLOBALS['value3'] as $ing) {
+                echo "res[".$GLOBALS['key3']."][\"index\"][".$numIng."] = \"".str_replace("'","",$ing)."\" ; "."\r\n" ;
+                $numIng++ ;
+            }
           }
 
+          }
+        }
       }
 
       ?>
